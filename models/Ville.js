c43173bb105f109a80100751f6db33c31c8b786d
23karim <karim.kekli@esprit.tn>
2024-07-24 13:23:02 +0200

    add ville model

---
+++ models/Ville.js
@@ -0,0 +1,10 @@
+class VilleModel {
+    constructor(id, nom, description, image) {
+        this.id = id || '';
+        this.nom = nom || '';
+        this.description = description || '';
+        this.image = image || '';
+    }
+}
+
+export default VilleModel;
